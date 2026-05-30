e156faa388a6eb7e7256d00519b64350608bc308
Mayank Mishra <mi5294759@gmail.com>
2026-05-30 11:08:30 +0530

    Add files via upload

---
+++ Day 1/getInput.cpp
@@ -0,0 +1,16 @@
+/* cin & cout is a globle scope object,
+       that is used to take input from the user and display output to the user respectively. */
+
+#include <iostream>
+using namespace std ;
+int main(){
+
+// Code  for get Input from user
+    int age ;
+    cout << "Enter Your Age : " ;
+    cin >> age ;
+    cout << "Your Age is : " << age << endl ;
+
+
+    return 0 ;
+}
